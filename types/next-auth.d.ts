b4da3f800038c28e4b2d6c16297f043776f6a1b3
Alex Rossi <alex.rossi@crdbbank.co.tz>
2024-04-21 16:46:12 +0300

    wip

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,13 +1,28 @@
-import NextAuth from "next-auth";
+// import NextAuth from "next-auth";
 
-declare module "next-auth" {
+// declare module "next-auth" {
+//   /**
+//    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+//    */
+//   interface Session {
+//     user: {
+//       /** The user's name. */
+//       uid: string;
+//     };
+//   }
+//}
+
+// types/next-auth.d.ts
+import 'next-auth';
+
+declare module 'next-auth' {
   /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   * This example extends the session to include user id as a string.
    */
   interface Session {
+    id?: string;
     user: {
-      /** The user's name. */
       uid: string;
-    };
+    } & DefaultSession['user'];
   }
 }
